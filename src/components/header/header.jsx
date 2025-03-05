1c564632302300b634a3f31fd419ab3040157c1f
sk1F25 <sk1fplaygames@gmail.com>
2025-03-05 18:25:05 +0300

    ✨ Update header and home page with profile button and minor text changes

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,27 +1,34 @@
 import { Link } from "react-router-dom";
+import avatar from "./avatar.jpg";
 
 export function Header() {
   return (
     <header className="w-full py-4 px-72 bg-gray-900 shadow-md flex justify-between">
-      <Link to="/" className="text-2xl font-bold text-white">
-        GuessTheFlag
+      <Link to="/" className="text-2xl font-bold text-white flex items-center">
+        GuessDaFlag
       </Link>
-      <nav className="flex items-center justify-between">
-        <div className="flex items-center space-x-6">
-          <Link
-            to="/game"
-            className="text-gray-300 hover:text-white transition-colors"
-          >
-            Play
-          </Link>
-          <Link
-            to="/leaderboard"
-            className="text-gray-300 hover:text-white transition-colors"
-          >
-            Leaderboard
-          </Link>
-        </div>
-      </nav>
+      <div className="flex items-center justify-between gap-10">
+        <nav>
+          <div className="flex items-center space-x-6">
+            <Link
+              to="/game"
+              className="text-gray-300 hover:text-white transition-colors"
+            >
+              Play
+            </Link>
+            <Link
+              to="/leaderboard"
+              className="text-gray-300 hover:text-white transition-colors"
+            >
+              Leaderboard
+            </Link>
+          </div>
+        </nav>
+        <button className="flex items-center gap-2 text-gray-300 hover:text-white transition-colors">
+          <span>Профиль</span>
+          <img src={avatar} className="w-8 h-8 rounded-full object-cover"></img>
+        </button>
+      </div>
     </header>
   );
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,7 +5,7 @@ export function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center pt-40">
-      <h1 className="text-4xl font-bold mb-8">GuessTheFlag</h1>
+      <h1 className="text-4xl font-bold mb-8">GuessDaFlag</h1>
       <button
         onClick={() => navigate("/game")}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
