b29cfcf68b30f98bc0fc80a4c4021fff76fb2136
sk1F25 <sk1fplaygames@gmail.com>
2025-03-05 19:21:13 +0300

    🎉 add supabase integration and fetch flags data

--- src/pages/game/game.jsx
+++ src/pages/game/game.jsx
@@ -1,7 +1,20 @@
 import clsx from "clsx";
 import { US, RU, CN, JP, GB } from "country-flag-icons/react/3x2";
+import { useEffect } from "react";
+import { supabase } from "../../api/supabase";
 
 export function Game({ className }) {
+  useEffect(() => {
+    const fetchFlags = async () => {
+      const { data, error } = await supabase.from("flags").select("*");
+
+      if (error) console.error(error);
+      else console.log("Флаги:", data);
+    };
+
+    fetchFlags();
+  }, []);
+
   return (
     <div
       className={clsx(className, "max-w-lg flex flex-col justify-center pt-40")}
